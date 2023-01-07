8969267d9c42110a19233245b4e9595c7d56ea79
brynden <brynden.bielefeld@gmail.com>
2023-01-07 14:03:15 -0600

    Created simple sidebar for navigation, removed footer.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { ReportsComponent } from './views/reports/reports.component';
 
 const routes: Routes = [
     {
         path: "dashboard",
         component: DashboardComponent
+    },
+    {
+        path: "reports",
+        component: ReportsComponent
     }
 ];
 
